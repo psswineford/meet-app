d8e0b22d7dfc9b67c9a24438ddadb8862f4f3f76
Shawn Swineford <psswineford@gmail.com>
2021-11-22 13:48:04 -0800

    end of task 4.2 rev1

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -108,3 +108,57 @@ module.exports.getAccessToken = async (event) => {
         };
       });
   };
+
+
+  module.exports.getCalendarEvents = async (event) => {
+    // The values used to instantiate the OAuthClient are at the top of the file
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );
+      // declare access token variable
+      //const code = decodeURIComponent(`${event.pathParameters.code}`);
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({access_token});
+    
+      return new Promise((resolve, reject) => {
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+      })
+        .then((results) => {
+          // Respond with OAuth token 
+          return {
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ events: results.data.items}),
+          };
+        })
+        .catch((err) => {
+          // Handle error
+          console.error(err);
+          return {
+            statusCode: 500,
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(err),
+          };
+        });
+    };
