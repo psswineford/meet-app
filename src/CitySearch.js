15dec3e24fc6fbbe162536219368abbe51b44426
Shawn Swineford <psswineford@gmail.com>
2021-11-29 11:52:22 -0800

    Section 4.4 halfway
    Halfway mark through the section just backing up files.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -20,7 +20,10 @@ class CitySearch extends Component {
         this.setState({
             query: suggestion
         });
+
+        this.props.updateEvents(suggestion);
     }
+
     render() {
         return (
             <div className="CitySearch">
@@ -37,7 +40,7 @@ class CitySearch extends Component {
                             onClick={() => this.handleItemClicked(suggestion)}
                         >{suggestion}</li>
                     ))}
-                    <li>
+                    <li onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
                 </ul>
