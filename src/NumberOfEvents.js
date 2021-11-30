0d2fa3c622b5729a79bfa79e606eadbe95d12e4a
Shawn Swineford <psswineford@gmail.com>
2021-11-30 15:08:02 -0800

    passing data to number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -14,6 +14,7 @@ class NumberOfEvents extends Component {
             numberOfEvents: number,
           });
         }
+        this.props.updateNumberOfEvents(number);
       };
 
     render() {
