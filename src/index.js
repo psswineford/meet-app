162b5342154394ceb385f85b5c0de84b8ad60f5c
Shawn Swineford <psswineford@gmail.com>
2021-12-07 11:10:15 -0800

    Task 4-6 implemented atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('03f663f7d1f64a10ab69c49a40d03404').install();
